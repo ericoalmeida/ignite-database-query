f0e48718447586de4f98b0b91facb7ae1a938e03
Erico Almeida <erico.silva@invillia.com>
2021-06-16 04:06:08 -0400

    Users queries are done

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,25 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository
+      .findOne({
+        where: { id: user_id },
+        relations: ['games']
+      });
+
+    return user as User
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository
+      .query('SELECT * FROM users ORDER BY users.first_name');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository
+      .query('SELECT * FROM users WHERE users.first_name ILIKE $1 AND users.last_name ILIKE $2 ', [first_name, last_name]);
   }
 }
